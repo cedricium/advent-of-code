ce2c42c00057d378f257bddbaf62d51f470a7a8e
Cedricium <amaya.cedric@gmail.com>
2022-12-03 12:13:29 -0800

    2022 Day 3 pt. 1 solution

--- 2022/day3/main.go
+++ 2022/day3/main.go
@@ -8,7 +8,50 @@ import (
 
 func main() {
 	input, _ := os.Open("input.txt")
-	scanner = bufio.NewScanner(input)
+	scanner := bufio.NewScanner(input)
+	sum := 0
 
-	fmt.Println("solution: ")
+	open := scanner.Scan()
+	for open {
+		rucksack := scanner.Text()
+		left := rucksack[:len(rucksack)/2]
+		right := rucksack[len(rucksack)/2:]
+
+		common := findCommonItem(left, right)
+		priority := calculatePriority(common)
+		sum += priority
+
+		open = scanner.Scan()
+	}
+
+	fmt.Println(sum)
+}
+
+func findCommonItem(a, b string) (common rune) {
+	set := map[rune]bool{}
+
+	for _, char := range a {
+		set[char] = true
+	}
+
+	for _, char := range b {
+		_, found := set[char]
+		if found {
+			common = char
+			break
+		}
+	}
+
+	return
+}
+
+// Given char, returns the corresponding priority integer as outlined in the
+// instructions, where lowercase a-z should return 1-26 and A-Z returns 27-52.
+// The int value of char is used to determine the casing.
+func calculatePriority(char rune) int {
+	if char > 90 { // lowercase
+		return int(char) - 96
+	} else { // uppercase
+		return int(char) - 38
+	}
 }
