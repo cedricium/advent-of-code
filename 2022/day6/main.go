556e52dbb34e8a46b771b8eb4be8b05c1a1ea672
Cedricium <amaya.cedric@gmail.com>
2022-12-06 19:34:39 -0800

    2022 Day 6 pt. 1 setup + solution

---
+++ 2022/day6/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+const markerSequenceLength = 4
+
+func main() {
+	input, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	index := 0
+	var buffer string
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		buffer += scanner.Text()
+	}
+
+	for i := 0; i < len(buffer); i++ {
+		seen := map[string]bool{}
+		seen[string(buffer[i])] = true
+
+		for j := i + 1; j < len(buffer); j++ {
+			if i >= markerSequenceLength-1 && len(seen) >= markerSequenceLength {
+				index = j
+				break
+			}
+
+			if !seen[string(buffer[j])] {
+				seen[string(buffer[j])] = true
+			} else {
+				break
+			}
+		}
+
+		if index > 0 {
+			break
+		}
+	}
+
+	fmt.Println(index)
+}
